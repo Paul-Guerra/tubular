cf12314a63aceca705c5b4ceece6672835676d0f
Paul Guerra <paulrguerra@gmail.com>
2017-12-10 23:08:14 -0500

    update test script

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 # /bin/bash
 # python -m unittest discover -p *_test.py
-coverage run --omit='.*' -m unittest discover -p *_test.py 
+
+# If this geta more complicated we can move this to a .coveragerc config file
+coverage run \
+  --omit='.python_environment/*' \
+  --source=. \
+  -m unittest discover -p *_test.py 
 coverage report -m
