1aaef5d228e855ca33e70828d29dcf3d5ecb9515
Paul Guerra <paulrguerra@gmail.com>
2017-12-12 23:30:52 -0500

    download and convert video

--- downloader.py
+++ downloader.py
@@ -14,7 +14,7 @@ class Downloader(object):
       if not os.path.exists(self.__temp_dir):
         os.makedirs(self.__temp_dir)
     except Exception as e:
-      logger.warn('Could not open {}'.format(self.__temp_dir))
+      logger.error('Could not open {}'.format(self.__temp_dir))
       self.__last_run = {}
 
     try:
@@ -28,13 +28,35 @@ class Downloader(object):
     try:
       write_dict_as_json(self.__last_run, self.__last_run_path)
     except Exception as e:
-      logger.warn('Could not write {}'.format(self.__last_run_path))
+      logger.error('Could not write {}'.format(self.__last_run_path))
 
   def run(self):
-    for show in self.subscriptions:
-      self.download_episodes(show)
+    for show in self.subscriptions.shows:
+      self.__download_episodes(show)
   
-  # def __download_episodes(self, show):
-    # for episode in show.episodes:
-      
+  def __get_new_episodes(self, show):
+    return [show.episodes[0]]
 
+  def __download_episodes(self, show):
+    
+    opts = {
+      'nocheckcertificate': True,
+      'keepvideo': False,
+      'outtmpl': 'tmp/%(id)s.%(ext)s',
+      'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '128',
+      }],
+      'logger': logger,
+      'progress_hooks': [onUpdate]
+    }
+
+    episodes = self.__get_new_episodes(show)
+    urls = list(map(lambda e: e.web_page, episodes))
+    with youtube_dl.YoutubeDL(opts) as ydl:
+      logger.info('Initiating download for urls {}'.format(urls))
+      ydl.download(urls)
+
+def onUpdate(u):
+  logger.info('{}: {}'.format(u['status'], u))
