f71ad4c9c474f653edb9076bb3a6b2c3f2c14018
Paul Guerra <paulrguerra@gmail.com>
2017-12-14 14:04:37 -0500

    serialize last run info and only DL new episodes

--- downloader.py
+++ downloader.py
@@ -1,45 +1,38 @@
 import os
+import ntpath
 import logging
 import youtube_dl
-from utils.files import write_dict_as_json
+from utils.files import write_dict_as_json, open_json_as_dict
 logger = logging.getLogger('tubular')
 
 class Downloader(object):
 
   def __init__(self, subscriptions):
     self.subscriptions = subscriptions
-    self.__last_run_path = 'last_run.json'
-    self.__temp_dir = 'tmp/'
+    self._last_run_path = 'last_run.json'
+    self._temp_dir = 'tmp/'
     try:
-      if not os.path.exists(self.__temp_dir):
-        os.makedirs(self.__temp_dir)
+      if not os.path.exists(self._temp_dir):
+        os.makedirs(self._temp_dir)
     except Exception as e:
-      logger.error('Could not open {}'.format(self.__temp_dir))
-      self.__last_run = {}
+      logger.error('Could not open {}'.format(self._temp_dir))
 
     try:
-      if not os.path.exists(self.__temp_dir):
-        os.makedirs(self.__temp_dir)
+      if not os.path.exists(self._temp_dir):
+        os.makedirs(self._temp_dir)
     except Exception as e:
       logger.exception(str(e))
       exit()
 
-  def __write_last_run(self):
-    try:
-      write_dict_as_json(self.__last_run, self.__last_run_path)
-    except Exception as e:
-      logger.error('Could not write {}'.format(self.__last_run_path))
-
   def run(self):
+    self._empty_temp_dir()    
     for show in self.subscriptions.shows:
-      self.__download_episodes(show)
-  
-  def __get_new_episodes(self, show):
-    return [show.episodes[0]]
-
-  def __download_episodes(self, show):
+      self._download_episodes(show)
+    self._write_last_run(_ids_from_tmp_dir(self._temp_dir))
     
-    opts = {
+  @property
+  def download_options(self):
+    return {
       'nocheckcertificate': True,
       'keepvideo': False,
       'outtmpl': 'tmp/%(id)s.%(ext)s',
@@ -49,15 +42,56 @@ class Downloader(object):
         'preferredquality': '128',
       }],
       'logger': logger,
-      'progress_hooks': [onUpdate]
+      'progress_hooks': [_onUpdate]
     }
+  
+  def _write_last_run(self, ids):
+    try:
+      write_dict_as_json({'ids': ids}, self._last_run_path)
+    except Exception as e:
+      logger.error('Could not write {}'.format(self._last_run_path))
+  
+  def _get_new_episodes(self, show):
+    try:
+      last_run_ids = set(open_json_as_dict(self._last_run_path)['ids'])
+      episode_ids = set(show.episode_ids)
+      new_episodes = episode_ids - last_run_ids
+      return show.get_episodes_by_id(list(new_episodes))
+    except Exception as e:
+      logger.warn('Could not determine new episodes: Error: {}', e)
+  
+  def _empty_temp_dir(self):
+    logger.info('Emptying {}'.format(self._temp_dir))
+    try:
+      fileList = os.listdir(self._temp_dir)
+      for fileName in fileList:
+        os.remove(self._temp_dir + '/' + fileName)
+    except OSError as e:
+      logger.warn('Cannot empty {}. Error: {}'.format(self._temp_dir, e))
 
-    episodes = self.__get_new_episodes(show)
+  def _download_episodes(self, show):
+    episodes = self._get_new_episodes(show)
     urls = list(map(lambda e: e.web_page, episodes))
-    with youtube_dl.YoutubeDL(opts) as ydl:
+    with youtube_dl.YoutubeDL(self.download_options) as ydl:
       logger.info('Initiating download for urls {}'.format(urls))
       ydl.download(urls)
+    logger.info('Downloads complete')
 
-def onUpdate(u):
+def _ids_from_tmp_dir(folder):
+  try:
+    file_list = os.listdir(folder)    
+  except OSError as e:
+    logger.warn(str(e))
+    file_list = []
+
+  return list(map(_get_id_from_path, file_list))
+
+def _get_id_from_path(path=''):
+  '''Extract the video id from the file path'''
+  if type(path) is not str or path is '':
+    return
+  return ntpath.basename(path).split('.')[0]
+
+def _onUpdate(u):
   if u['status'] is 'finished':
-    logger.info('{}: {}'.format(u['status'], u))
+    logger.info('download {}: {}'.format(u['status'], u))
