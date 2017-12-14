a420bd8c0b12965d5855e1d8f39f61b3c6c10c09
Paul Guerra <paulrguerra@gmail.com>
2017-12-14 14:03:42 -0500

    index episodes by id

--- show.py
+++ show.py
@@ -2,10 +2,40 @@ class Show(object):
 
   def __init__(self, title='Default Title', episodes=[]):
     self.title = title
-    self.episodes = episodes
-
+    self.__episodes = episodes
+    self.__episodes_by_id = {}
+    self.__index_episodes(episodes)
+  
   def __str__(self):
-    return 'Title: {}, Latest Episode: {},  Episodes[{}]'.format(self.title, self.episodes[0].title, len(self.episodes))
+    return 'Title: {}, Latest Episode: {},  Episodes[{}]'.format(self.title, self.__episodes[0].title, len(self.__episodes))
 
   def __repr__(self):
     return self.__str__()
+
+  def __index_episodes(self, episodes):
+    for e in episodes:
+      self.__episodes_by_id[e.id] = e
+  
+  @property
+  def episodes(self):
+    return self.__episodes
+
+  @property
+  def episodes_by_id(self):
+    return self.__episodes_by_id
+
+  @property
+  def episode_ids(self):
+    return dict.keys(self.episodes_by_id)
+
+  def get_episodes_by_id(self, ids):
+    return list(map(
+      lambda i: self.episodes_by_id[i],
+      ids
+    ))
+
+  # def get_new_episodes(self, previous_episode_ids):
+  #   # last_run_ids = set(open_json_as_dict(self._last_run_path)['ids'])
+  #   episode_ids = set(self.episode_ids)
+  #   new_episodes = episode_ids - previous_episode_ids
+  #   return self.get_episodes_by_id(list(new_episodes))
