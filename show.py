6335b2f1f8e1346ac9588e342ec46f6308157b88
Paul Guerra <paulrguerra@gmail.com>
2017-12-20 12:38:56 -0500

    misc linting

--- show.py
+++ show.py
@@ -1,42 +1,56 @@
+'''
+Show class an related utility functions
+'''
+
 import os
+import logging
+import utils.files as uf
 from episode import Episode, parse_entry
 
-class Show(object):
+logger = logging.getLogger('tubular')
 
-    def __init__(self, id, title, episodes=[]):
+class Show(object):
+    '''
+    Class for a show object as populated from crawl responses
+    '''
+
+    def __init__(self, id, title, episodes):
         self.title = title
         self.__id = id
-        self.__episodes = episodes
+        self.__episodes = episodes or []
         self.__episodes_by_id = {}
         self.__index_episodes(episodes)
-    
-    def __str__(self):
-        return 'Title: {}, Latest Episode: {},    Episodes[{}]'.format(self.title, self.__episodes[0].title, len(self.__episodes))
-
-    def __repr__(self):
-        return self.__str__()
 
     def __index_episodes(self, episodes):
         for e in episodes:
             self.__episodes_by_id[e.id] = e
     
     @property
+    def id(self):
+        '''Read-only id of the show'''
+        return self.__id
+
+    @property
     def episodes(self):
+        ''' Read-only access to episodes property'''
         return self.__episodes
 
     @property
     def downloaded_episodes(self):
-        return filter(
+        '''Returns a list of episodes marked as downloaded'''
+        return list(filter(
             lambda e: e.downloaded is True,
             self.__episodes
-        )
+        ))
 
     @property
     def episodes_by_id(self):
+        '''Returns a dictionary of episodes using their id as the key'''
         return self.__episodes_by_id
 
     @property
     def episode_ids(self):
+        '''Returns all episode ids'''
         return dict.keys(self.episodes_by_id)
 
     def get_episodes_by_id(self, ids):
@@ -53,6 +67,7 @@ class Show(object):
 
 
 def open_show_from_file(path):
+    '''Return a show object using data stored in the file found at path'''
     try:
         data = uf.open_json_as_dict(path)
     except Exception as e:
@@ -62,6 +77,8 @@ def open_show_from_file(path):
     return Show(id=data["id"], title=data['title'], episodes=data['episodes'])
 
 def get_archived_shows(dir='data/'):
+    '''Shows previously downloaded and published'''
+
     shows = {}
     fileList = os.listdir(dir)
     for f in fileList:
@@ -72,6 +89,8 @@ def get_archived_shows(dir='data/'):
 
 
 def feeds_to_shows(feeds):
+    '''Converts feeds data into a dictionary or show objects'''
+  
     shows = {}
     for f in feeds:
          shows[f.manifest_item.id] = Show(
@@ -83,6 +102,11 @@ def feeds_to_shows(feeds):
     return shows
 
 def get_episodes_from_entries(feed_response):
+    '''
+    Return a list of episodes from the provided entries 
+    that include one the specified "include" strings
+    '''
+
     entries_dict = list(map(parse_entry, feed_response.entries))
     episodes = list(map(Episode, entries_dict))
     includes = list(map(
@@ -95,15 +119,14 @@ def get_episodes_from_entries(feed_response):
     ))
 
 def title_is_included(title, includes):
+    '''Returns True if one of the include list strings is in the title'''
     for i in includes:
         if i in title:
             return True 
     return False
 
 def shows_with_new_episodes(available_shows, archived_shows):
-    '''
-    Returns a distionary of show ids that map to show object containing new episodes
-    '''
+    '''Returns a dictionary of show ids that map to show object containing new episodes'''
     shows = {}
     for show in available_shows:
         episodes = new_episodes(show, archived_shows[show.id])
@@ -111,8 +134,6 @@ def shows_with_new_episodes(available_shows, archived_shows):
             shows[show.id] = Show(show.id, show.title, episodes)
 
     return shows
-# def episode_ids(show):
-#     return list(map(lambda e: e.id, show.episodes))
 
 def new_episodes(available_show, archived_shows):
     '''
@@ -123,16 +144,3 @@ def new_episodes(available_show, archived_shows):
         return available_show
     
     archived_show = archived_shows[available_show.id]
-    
-    # for episode in available_show.episodes
-
-
-        
-
-
-    # archived_show = archived_shows[available_show.id]
-    # available_episode_ids = episode_ids(available_show.episodes)
-    # archived_episode_ids = episode_ids(archived_show.episodes)
-    # # todo: overload set comparison so that we can use episode objects directly in sets and not a list of ids
-
-    # set() - set(archived_show.episodes)
