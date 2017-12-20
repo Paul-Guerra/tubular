2931341fdcbef6b90b5d1ff5b9b2eb85a2c09832
Paul Guerra <paulrguerra@gmail.com>
2017-12-19 23:58:39 -0500

    fix indenting

--- utils/files.py
+++ utils/files.py
@@ -6,25 +6,25 @@ import ntpath
 logger = logging.getLogger('tubular')
 
 def open_json_as_dict(path=''):
-  if path is '' or type(path) is not str:
-    raise Exception('Bad path: "{}"'.format(path))
+    if path is '' or type(path) is not str:
+        raise Exception('Bad path: "{}"'.format(path))
 
-  if os.path.exists(path):
-    with open(path, 'r') as f:
-      return json.load(f)
-  else:
-    raise Exception('Path does not exist: "{}"'.format(path))
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            return json.load(f)
+    else:
+        raise Exception('Path does not exist: "{}"'.format(path))
 
 def write_dict_as_json(obj, path):
-  with open(path, 'w') as fp:
-    json.dump(obj, fp)
+    with open(path, 'w') as fp:
+        json.dump(obj, fp)
 
 def touch(path):
-  dirname = ntpath.dirname(path)
-  file_name = ntpath.basename(path)
-  if not os.path.exists(path):
-    if len(dirname) > 0:
-      os.makedirs(dirname)
-      
-    if len(file_name) > 0:
-      open(path, 'w').close()
+    dirname = ntpath.dirname(path)
+    file_name = ntpath.basename(path)
+    if not os.path.exists(path):
+        if len(dirname) > 0:
+            os.makedirs(dirname)
+            
+        if len(file_name) > 0:
+            open(path, 'w').close()
