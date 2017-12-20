cbe98d19910e6b1258ac24a651c0898022d98848
Paul Guerra <paulrguerra@gmail.com>
2017-12-19 23:57:55 -0500

    indenting

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from utils.files import open_json_as_dict
 logger = logging.getLogger('tubular')
 
 def load():
-  try:
-    return open_json_as_dict('config.json')
-  except Exception as e:
-    logger.critical('Error loading app config: {}'.format(str(e)))
-    exit()
+    try:
+        return open_json_as_dict('config.json')
+    except Exception as e:
+        logger.critical('Error loading app config: {}'.format(str(e)))
+        exit()
 
