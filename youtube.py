4ea78bce51303fc48802f7b1cf8a848bcfebf809
Paul Guerra <paulrguerra@gmail.com>
2017-12-19 23:56:33 -0500

    fix indenting

--- youtube.py
+++ youtube.py
@@ -5,28 +5,28 @@ from manifest_item import ManifestItem
 logger = logging.getLogger('tubular')
 
 def crawl(manifest):
-  return list(filter(lambda r: r is not None, map(fetch, manifest)))
+    return list(filter(lambda r: r is not None, map(fetch, manifest)))
 
 
 def fetch(item):
-  url = item.url
-  try:
-    logger.info('Making request to {}'.format(url))
-    response = requests.get(url, timeout=5)
-    if response.status_code is not 200:
-      logger.warn('Received non 200 response code for {}. Status Code:{}'.format(url, response.status_code))
-      return None
-    return CrawlResponse(response, item)
-  except Exception:
-    logger.exception('Error fetching response: {}'.format(url))
+    url = item.url
+    try:
+        logger.info('Making request to {}'.format(url))
+        response = requests.get(url, timeout=5)
+        if response.status_code is not 200:
+            logger.warn('Received non 200 response code for {}. Status Code:{}'.format(url, response.status_code))
+            return None
+        return CrawlResponse(response, item)
+    except Exception:
+        logger.exception('Error fetching response: {}'.format(url))
 
 
 def manifest(config):
-  if config is None:
-    return False
-  items = []
-  for chan in config['channels']:
-    items.append(ManifestItem(chan, config['youtube']['user_url']))
+    if config is None:
+        return False
+    items = []
+    for chan in config['channels']:
+        items.append(ManifestItem(chan, config['youtube']['user_url']))
 
-  return items
+    return items
 
