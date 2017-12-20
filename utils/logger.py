e70c1f89d1a12fe4500f07362e22cc95ac17a890
Paul Guerra <paulrguerra@gmail.com>
2017-12-19 23:58:45 -0500

    fix indenting

--- utils/logger.py
+++ utils/logger.py
@@ -6,18 +6,18 @@ import logging.config
 logConfigFile = 'utils/logger.json'
 
 def initLogging(path=logConfigFile):
-  if os.path.exists(path):
-    initLoggingFromFile(path)
-  else:
-    initBasicLogging()
+    if os.path.exists(path):
+        initLoggingFromFile(path)
+    else:
+        initBasicLogging()
 
 def initLoggingFromFile(path=logConfigFile):
-  try:
-    with open(path, 'r') as f:
-      config = json.load(f)
-    logging.config.dictConfig(config)
-  except (IOError, OSError):
-    initBasicLogging()
+    try:
+        with open(path, 'r') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    except (IOError, OSError):
+        initBasicLogging()
 
 def initBasicLogging():
-  logging.basicConfig()
+    logging.basicConfig()
