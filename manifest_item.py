d7eb0b9ecab1df3e5ae94dd723521d7712f7b25f
Paul Guerra <paulrguerra@gmail.com>
2017-12-22 15:57:56 -0500

    indenting

--- manifest_item.py
+++ manifest_item.py
@@ -2,24 +2,25 @@ from slugify import slugify
 
 class ManifestItem(object):
   
-  def __init__(self, channel, url_base):
-    self.__id = slugify(channel['title'])
-    self.__channel = channel
-    self.__url_base = url_base
+    def __init__(self, channel, url_base):
+        super().__init__()
+        self.__id = slugify(channel['title'])
+        self.__channel = channel
+        self.__url_base = url_base
   
-  @property
-  def url(self):
-    return '{}{}'.format(self.__url_base, self.__channel['user'])
-    
-  @property
-  def include(self):
-    return self.__channel['include']
+    @property
+    def url(self):
+        return '{}{}'.format(self.__url_base, self.__channel['user'])
 
-  @property
-  def title(self):
-    return self.__channel['title']
+    @property
+    def include(self):
+        return self.__channel['include']
 
-  @property
-  def id(self):
-    return self.__id
+    @property
+    def title(self):
+        return self.__channel['title']
+
+    @property
+    def id(self):
+        return self.__id
     
