4d119d3d1b8984820a962e84cfd0de8a735e06b7
Paul Guerra <paulrguerra@gmail.com>
2017-12-22 16:03:17 -0500

    finish downloading new episodes only

--- downloader.py
+++ downloader.py
@@ -2,19 +2,16 @@ import os
 import ntpath
 import logging
 import youtube_dl
-from utils.files import write_dict_as_json, open_json_as_dict, touch
+from show import Show
+
 logger = logging.getLogger('tubular')
 
 class Downloader(object):
 
-    def __init__(self, available_shows):
-        self.available_shows = available_shows
-        self.__last_run_path = 'last_run.json'
+    def __init__(self):
         super().__init__()
 
         self.__temp_dir = 'tmp/'
-        self.__downloaded_ids = []
-        self.init_dl_tracking()
         try:
             if not os.path.exists(self.__temp_dir):
                 os.makedirs(self.__temp_dir)
@@ -22,61 +19,45 @@ class Downloader(object):
             logger.exception(str(e))
             exit()
 
-    def init_dl_tracking(self):
-        default_dls = {'ids': []}
-        try:
-            if not os.path.exists(self.__last_run_path):
-                touch(self.__last_run_path)
-                write_dict_as_json(default_dls, self.__last_run_path)
-            else:
-                self.__downloaded_ids = open_json_as_dict(self.__last_run_path)['ids']
-        except Exception as e:
-            logger.exception(str(e))
+    def run(self, new_shows):
+        downloaded_shows = []
 
-    def run(self):
         self.__empty_temp_dir()
-        for _, show in self.available_shows.items():
-            self.__download_episodes(show)
-            self.__downloaded_ids += _ids_from_tmp_dir(self.__temp_dir)
-        self.__write_last_run(self.__downloaded_ids)
-    
-    @property
-    def download_options(self):
+        for _, show in new_shows.items():
+            new_episodes = self.download_episodes(show.episodes)
+            if new_episodes:
+                downloaded_shows.append(
+                    Show(show_id=show.id, title=show.title, episodes=new_episodes)
+                )
+
+        return downloaded_shows
+
+    def youtube_dl_options(self, episode):
+        # todo: see if option allows skipping the video download
         return {
-            'simulate': True,
+            'ffmpeg_location': '/usr/local/bin/ffmpeg',
             'nocheckcertificate': True,
             'keepvideo': False,
-            'outtmpl': '{dir}/%(id)s.%(ext)s'.format(dir=self.__temp_dir),
+            'outtmpl': '{dir}%(id)s.%(ext)s'.format(dir=self.__temp_dir),
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
                 'preferredquality': '128',
             }],
             'logger': logger,
-            'progress_hooks': [on_download_update]
+            'progress_hooks': [lambda u: on_download_update(episode, u)]
         }
-    
-    def __write_last_run(self, ids):
-        try:
-            # write_dict_as_json({'ids': ids}, self.__last_run_path)
-            write_dict_as_json({'ids': []}, self.__last_run_path)
-        except Exception as e:
-            logger.error('Could not write {}'.format(self.__last_run_path))
 
-    def __download_episodes(self, show):
-        try:
-            episodes = show.get_new_episodes(self.__downloaded_ids)
-            urls = list(map(lambda e: e.web_page, episodes))
-            with youtube_dl.YoutubeDL(self.download_options) as ydl:
-                logger.info('Initiating download for urls {}'.format(urls))
-                ydl.download(urls)
-            logger.info('Downloads complete')
-            self.__mark_episodes_downloaded(episodes)
-            return episodes
-        except Exception as e:
-            logger.exception('Could not download {}: Error: {}'.format(urls, e))
-        
-        return []
+    def download_episodes(self, episodes):
+            for e in episodes:
+                options = self.youtube_dl_options(e)
+                try:
+                    with youtube_dl.YoutubeDL(options) as ydl:
+                        logger.info('Initiating download for episode {}'.format(e))
+                        ydl.download([e.web_page])
+                except Exception as err:
+                    logger.exception(str(err))
+            return list(filter(lambda e: e.download_finished is True, episodes))
         
     def __empty_temp_dir(self):
         logger.info('Emptying {}'.format(self.__temp_dir))
@@ -87,29 +68,8 @@ class Downloader(object):
         except OSError as e:
             logger.warn('Cannot empty {}. Error: {}'.format(self.__temp_dir, e))
 
-    def __episode_tmp_path(self, episode):
-        return '{dir}/{name}.mp3'.format(dir=self.__temp_dir, name=episode.id)
-
-    def __mark_episodes_downloaded(self, episodes):
-        for e in episodes:
-            e.downloaded = True
-            e.tmp_path = self.__episode_tmp_path(e)
-
-def _ids_from_tmp_dir(folder):
-    try:
-        file_list = os.listdir(folder)        
-    except OSError as e:
-        logger.warn(str(e))
-        file_list = []
-
-    return list(map(_get_id_from_path, file_list))
-
-def _get_id_from_path(path=''):
-    '''Extract the video id from the file path'''
-    if type(path) is not str or path is '':
-        return
-    return ntpath.basename(path).split('.')[0]
-
-def on_download_update(u):
+def on_download_update(episode, u):
     if u['status'] is 'finished':
-        logger.info('download {}: {}'.format(u['status'], u))
+        logger.info('Download {}: Episode: "{}" ({})'.format(u['status'], episode.title, episode.id))
+        logger.info(f'Download Data: {u}')
+        episode.download_status = u
--- index.py
+++ index.py
@@ -1,43 +1,48 @@
 import platform
 import logging
-import config
+from config import load as load_config
 import youtube
 from show import (
-  Show,
-  feeds_to_shows,
-  open_show_from_file, 
-  get_archived_shows
-  )
-from episode import Episode, parse_entry
+    feeds_to_shows,
+    get_archived_shows,
+    shows_with_new_episodes
+)
 from downloader import Downloader
-from uploader import Uploader
 from utils.logger import initLogging
 import utils.files as uf
 
-initLogging()
-logger = logging.getLogger('tubular')
+def main():
+    initLogging()
+    logger = logging.getLogger('tubular')
 
-uf.touch('data/')
+    uf.touch('data/')
 
-logger.info('Starting Tubular')
-logger.debug('Running on Python {}'.format(platform.python_version()))
-logger.debug('Log level set to {}'.format(logger.level))
-config = config.load()
+    logger.info('Starting Tubular')
+    logger.debug('Running on Python {}'.format(platform.python_version()))
+    logger.debug('Log level set to {}'.format(logger.level))
+    config = load_config()
 
-feeds = youtube.crawl(youtube.manifest(config))
-available_shows = feeds_to_shows(feeds)
-archived_shows = get_archived_shows()
-# new_episodes = shows_with_new_episodes(available_shows, archived_shows)
-print('available_shows', available_shows)
-print('archived_shows', archived_shows)
-dl_manager = Downloader(available_shows)
-dl_manager.run()
+    feeds = youtube.crawl(youtube.manifest(config))
+    available_shows = feeds_to_shows(feeds)
+    archived_shows = get_archived_shows()
 
-# open_show_from_file('data/')
-'''
-# open_show_from_file() # creates default if file does not exist
-dl_manager = Downloader(available_shows, archived_shows)
-new_episodes = dl_manager.run()
-add new episodes to archive
-rewrite archive file
-'''
+    has_new_episodes = shows_with_new_episodes(available_shows, archived_shows)
+
+    dl_manager = Downloader()
+    downloaded_shows = dl_manager.run(has_new_episodes)
+    add_new_episodes(downloaded_shows, archived_shows)
+
+    print('archived_shows', archived_shows)
+
+    print(downloaded_shows[0].to_json())
+
+def add_new_episodes(downloaded_shows, archived_shows):
+    for show in downloaded_shows:
+        if show.id not in archived_shows:
+            archived_shows[show.id] = show
+        else:
+            archive = archived_shows[show.id]
+            archive.add_new_episodes(show.episodes)
+
+
+main()
