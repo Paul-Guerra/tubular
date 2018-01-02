ed0f9f80ff4217125b0ffccac099f2a6f450d314
Paul Guerra <paulrguerra@gmail.com>
2018-01-02 18:07:59 -0500

    update audio file and download status

--- episode.py
+++ episode.py
@@ -1,8 +1,12 @@
 '''
 Episode class and related helper function, parse_entry, used for coverting data from the XML feed
 '''
-
+import os
 import json
+import logging
+from utils.files import mkdir, change_ext
+
+logger = logging.getLogger('tubular')
 
 class Episode(object):
     '''
@@ -17,7 +21,8 @@ class Episode(object):
         self.__description = data['description']
         self.__video = data['video']
         self.__thumbnail = data['thumbnail']
-        self.download_status = {}
+        self.__audio_path = data['audio_path'] if 'audio_path' in data else ''
+        self.__download_status = {}
 
     def __hash__(self):
         return hash(self.id)
@@ -57,22 +62,54 @@ class Episode(object):
     def thumbnail(self):
         '''URL to the episode thumbnail'''
         return self.__thumbnail
+    
+    @property
+    def download_status(self):
+        '''Return the download status object'''
+        return self.__download_status
+
+    @download_status.setter
+    def download_status(self, val):
+        '''Update the download status object and file path for audio'''
+        self.__download_status = val
+        if 'filename' in val:
+            self.__audio_path = change_ext(val['filename'], '.mp3')
+
+    @property
+    def audio_path(self):
+        '''Read-only audio path'''
+        return self.__audio_path
 
     @property
     def download_finished(self):
+        '''Boolean inidcating if the download is finished'''
         if 'status' in self.download_status:
             return self.download_status['status'] == 'finished'
+        elif self.__audio_path:
+            return True
         else:
             return False
     
+    def archive_audio(self, dest):
+        try:
+            # logger.info(f'Archving Episode {self.audio_path}')
+            os.rename(self.audio_path, dest)
+            self.__audio_path = dest
+        except OSError as err:
+            logger.exception(str(err))
+
     def to_dict(self):
+        '''Properties as a dictionary'''
         return {
             'id': self.id,
             'title': self.title,
             'web_page': self.web_page,
+            'description': self.description,
             'video': self.video,
             'thumbnail': self.thumbnail,
+            'audio_path': self.audio_path,
         }
+
 
 def parse_entry(xmldict):
     '''
