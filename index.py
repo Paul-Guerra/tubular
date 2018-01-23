85c49a2c61455b14a997854145056ab4267a9302
Paul Guerra <paulrguerra@gmail.com>
2018-01-23 09:12:23 -0500

    add try catch and rename config variable to avoid collision with import

--- index.py
+++ index.py
@@ -17,29 +17,31 @@ import podcast
 def main():
     initLogging()
     logger = logging.getLogger('tubular')
+    try:
+        logger.info('Starting Tubular')
+        logger.debug(f'Running on Python {platform.python_version()}')
+        logger.debug(f'Log level set to {logger.level}')
+        app_config = load_config()
 
-    logger.info('Starting Tubular')
-    logger.debug(f'Running on Python {platform.python_version()}')
-    logger.debug(f'Log level set to {logger.level}')
-    config = load_config()
+        uf.touch(app_config['data_dir'])
+        uf.touch(app_config['audio_dir'])
 
-    uf.touch(config['data_dir'])
-    uf.touch(config['audio_dir'])
+        feeds = youtube.crawl(youtube.manifest(app_config))
+        available_shows = feeds_to_shows(feeds)
+        archived_shows = get_archived_shows(app_config['data_dir'])
 
-    feeds = youtube.crawl(youtube.manifest(config))
-    available_shows = feeds_to_shows(feeds)
-    archived_shows = get_archived_shows(config['data_dir'])
+        has_new_episodes = shows_with_new_episodes(available_shows, archived_shows)
 
-    has_new_episodes = shows_with_new_episodes(available_shows, archived_shows)
+        if len(has_new_episodes.keys()) is 0:
+            logger.info('No New episodes found. Exiting')
+            return
 
-    if len(has_new_episodes.keys()) is 0:
-        logger.info('No New episodes found. Exiting')
-        return
-
-    dl_manager = Downloader()
-    downloaded_shows = dl_manager.run(has_new_episodes)
-    add_new_episodes(downloaded_shows, archived_shows)
-    write_archive(archived_shows, config)
+        dl_manager = Downloader()
+        downloaded_shows = dl_manager.run(has_new_episodes)
+        add_new_episodes(downloaded_shows, archived_shows)
+        write_archive(archived_shows, app_config)
+    except Exception as err:
+        logger.exception(str(err))
 
 def write_archive(archived_shows, config):
     logger = logging.getLogger('tubular')    
@@ -63,6 +65,8 @@ def add_new_episodes(downloaded_shows, archived_shows):
             archive.add_new_episodes(show.episodes)
 
 # main()
+
+initLogging()
 config = load_config()
 archived_shows = get_archived_shows(config['data_dir'])
 write_archive(archived_shows, config)
