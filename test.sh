eec462830d9a2e21910f6303ceac60f632b50708
Paul Guerra <paulrguerra@gmail.com>
2018-01-30 18:19:46 -0500

    make tests/ a module

--- test.sh
+++ test.sh
@@ -3,12 +3,13 @@
 # python -m unittest discover -p *_test.py
 
 # If this gets more complicated we can move this to a .coveragerc config file
-# coverage run \
-#   --omit='.venv/*' \
-#   --source=. \
-#   -m unittest discover -p *_test.py 
-# coverage report -m
+coverage run \
+  --omit='.venv/*' \
+  --source=. \
+  -m unittest discover -p *_test.py 
+coverage report -m
 
 # run coverage but only test one file
-coverage run tests/utils/files_test.py
-coverage report -m
+# coverage run tests/utils/files_test.py
+# coverage run  --omit='.venv/*' tests/youtube_test.py
+# coverage report -m
