11b078aed18efef99131d081e84558a71691a5f9
Paul Guerra <paulrguerra@gmail.com>
2018-02-15 18:45:43 -0500

    move each service into their own folders

--- tubular/workers.py
+++
@@ -1,8 +0,0 @@
-'''A collection of worker child processes for the manin proess to manage'''
-
-class Workers(object):
-    def __init__(self):
-        object(self)
-        self.__pool = []
-    
-    
